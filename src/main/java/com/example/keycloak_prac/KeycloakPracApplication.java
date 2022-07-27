8a24eec311121f008de6ad6b81e2fd985684a69b
ParkChaerim <qkrdbsk28@naver.com>
2022-07-27 10:35:12 +0900

    first commit

---
+++ src/main/java/com/example/keycloak_prac/KeycloakPracApplication.java
@@ -0,0 +1,13 @@
+package com.example.keycloak_prac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeycloakPracApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KeycloakPracApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/keycloak_prac/KeycloakPracApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.keycloak_prac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeycloakPracApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
