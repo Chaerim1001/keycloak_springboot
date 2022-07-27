ebc5b300f1d0e5f81550d6a55adcfb3588182e50
ParkChaerim <qkrdbsk28@naver.com>
2022-07-27 19:21:29 +0900

    password encoder with spring security

--- src/main/java/com/example/security/SpringSecurityConfig.java
+++ src/main/java/com/example/security/SpringSecurityConfig.java
@@ -1,11 +1,13 @@
 package com.example.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
@@ -15,7 +17,7 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
     UserDetailsServiceImpl userDetailsServiceImpl;
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userDetailsServiceImpl);
+        auth.userDetailsService(userDetailsServiceImpl).passwordEncoder((passwordEncoder()));
     }
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -25,5 +27,10 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
                 .formLogin().loginPage("/login")
                     .defaultSuccessUrl("/home")
                         .usernameParameter("username").passwordParameter("password");
+    }
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
     }
 }
