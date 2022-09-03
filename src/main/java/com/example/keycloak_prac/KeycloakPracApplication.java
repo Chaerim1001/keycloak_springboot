142ad5a042a3fd639f818e6c3bca709431d4016c
chaerim <qkrdbsk28@naver.com>
2022-09-03 21:03:27 +0900

    mysql setting

--- src/main/java/com/example/keycloak_prac/KeycloakPracApplication.java
+++ src/main/java/com/example/keycloak_prac/KeycloakPracApplication.java
@@ -4,14 +4,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 @ComponentScan({"com.example.controller", "com.example.service", "com.example.security"})
 @EntityScan("com.example.entity")
 @EnableJpaRepositories("com.example.repository")
+@PropertySource("classpath:/env.properties")
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class KeycloakPracApplication {
 
